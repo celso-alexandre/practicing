8698cc2baf845e0047c2183f5e6e4f0323600c39
Celso Alexandre <celsoalexandre@live.com>
2021-03-14 16:14:51 -0300

    rocketseat-nlw4: Day 3: Challengebox, app logic, Challenges context (React Context API)

--- rocketseat-nlw4/src/components/ExperienceBar.tsx
+++ rocketseat-nlw4/src/components/ExperienceBar.tsx
@@ -1,17 +1,31 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css';
 
 export default function ExperienceBar() {
+  const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
+  const toNextLevelPercentage = Math.round((currentExperience / experienceToNextLevel) * 100);
+
   return (
     <header className={styles.experienceBar}>
-      <span>0 xp</span>
+      <span>
+        {experienceToNextLevel}
+        {' '}
+        xp
+      </span>
       <div>
-        <div style={{ width: '50%' }} />
+        <div style={{ width: `${toNextLevelPercentage}%` }} />
 
-        <span className={styles.currentExperience} style={{ left: '50%' }}>
-          300xp
+        <span className={styles.currentExperience} style={{ left: `${toNextLevelPercentage}%` }}>
+          {currentExperience}
+          xp
         </span>
       </div>
-      <span>600 xp</span>
+      <span>
+        {experienceToNextLevel}
+        {' '}
+        xp
+      </span>
     </header>
   );
 }
